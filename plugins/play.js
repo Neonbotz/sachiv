73ae2290eade224251a1873a15c0ba582ab27a20
Agungx Devs <106319373+Neonbotz@users.noreply.github.com>
2023-04-09 12:04:02 +0700

    Update play.js

--- plugins/play.js
+++ plugins/play.js
@@ -1,42 +1,50 @@
-let yts = require('yt-search')
-let fetch = require('node-fetch')
-const { servers, yta, ytv } = require('../lib/y2mate')
-let handler = async (m, { conn, command, usedPrefix, text, isPrems, isOwner }) => {
-  if (!text) throw `uhm.. cari apa?\n\ncontoh:\n${usedPrefix + command} i see your monster`
-  let chat = global.db.data.chats[m.chat]
-  let results = await yts(text)
-  let vid = results.all.find(video => video.seconds < 3600)
-  if (!vid) throw 'Video/Audio Tidak ditemukan'
-  let isVideo = /2$/.test(command)
-  let yt = false
-  let usedServer = servers[0]
-  for (let i in servers) {
-    let server = servers[i]
-    try {
-      yt = await yta(vid.url, server)
-      yt2 = await ytv(vid.url, server)
-      usedServer = server
-      break
-    } catch (e) {
-      m.reply(`Server ${server} error!${servers.length >= i + 1 ? '' : '\nmencoba server lain...'}`)
-    }
+const { youtubedl, youtubedlv2, youtubedlv3 } = require('@bochilteam/scraper')
+const yts = require('yt-search')
+var handler = async (m, { conn, command, text, usedPrefix }) => {
+  if (!text) throw `Use example ${usedPrefix}${command} naruto blue bird`
+  await m.reply(wait)
+  let search = await yts(text)
+  let vid = search.videos[Math.floor(Math.random() * search.videos.length)]
+  if (!search) throw 'Video Not Found, Try Another Title'
+  let { title, thumbnail, timestamp, views, ago, url } = vid
+
+  let captvid = `╭──── 〔 Y O U T U B E 〕 ─⬣
+⬡ Judul: ${title}
+⬡ Durasi: ${timestamp}
+⬡ Views: ${views}
+⬡ Upload: ${ago}
+⬡ Link: ${url}
+╰────────⬣`
+  let buttons = [{ buttonText: { displayText: '📽VIDEO' }, buttonId: `.ytv ${url}` }]
+ let msg = await conn.sendMessage(m.chat, { image: { url: thumbnail }, caption: captvid, footer: author, buttons }, { quoted: m })
+
+  const yt = await await youtubedlv2(url).catch(async _ => await youtubedl(url)).catch(async _ => await youtubedlv3(url))
+const link = await yt.audio['128kbps'].download()
+  let doc = { 
+  audio: 
+  { 
+    url: link 
+}, 
+mimetype: 'audio/mp4', fileName: `${title}`, contextInfo: { externalAdReply: { showAdAttribution: true,
+mediaType:  2,
+mediaUrl: url,
+title: title,
+body: wm,
+sourceUrl: url,
+thumbnail: await(await conn.getFile(thumbnail)).data                                                                     
+                                                                                                                 }
+                       }
   }
-  if (yt === false) throw 'semua server gagal'
-  if (yt2 === false) throw 'semua server gagal'
-  let { dl_link, thumb, title, filesize, filesizeF } = yt
-  await conn.send3ButtonLoc(m.chat, await conn.resize(thumb, 280, 210), `
-*Judul:* ${title}
-*Ukuran File Audio:* ${filesizeF}
-*Ukuran File Video:* ${yt2.filesizeF}
-*Server y2mate:* ${usedServer}
- `.trim(), wm, `🎵 AUDIO ${filesizeF}`, usedPrefix + `yta ${vid.url}`, `📽 VIDEO ${yt2.filesizeF}`, usedPrefix + `yt ${vid.url}`, `🔍 SEARCH`, `.yts ${vid.url}`, m)
+
+  return conn.sendMessage(m.chat, doc, { quoted: m })
+	// return conn.sendMessage(m.chat, { document: { url: link }, mimetype: 'audio/mpeg', fileName: `${title}.mp3`}, { quoted: m})
+	// return await conn.sendFile(m.chat, link, title + '.mp3', '', m, false, { asDocument: true })
 }
 handler.help = ['play'].map(v => v + ' <pencarian>')
 handler.tags = ['downloader']
-handler.command = /^(dj|musik|song|lagu|p(lay)?)$/i
+handler.command = /^play$/i
 
-handler.exp = 3
-handler.limit = false
-handler.register = false
+handler.exp = 0
+handler.limit = true
 
 module.exports = handler
