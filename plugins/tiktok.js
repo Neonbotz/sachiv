057a0c952418d86ce4db2b1eb96cbbfeb84b3745
Agungx Devs <106319373+Neonbotz@users.noreply.github.com>
2023-04-09 12:02:41 +0700

    Update tiktok.js

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -1,16 +1,50 @@
-const { tiktokdl, tiktokdlv2, tiktokdlv3 } = require('@bochilteam/scraper')
-let handler = async (m, { conn, args, usedPrefix, command, isOwner }) => {
-	if (!args[0]) throw `Link tiktoknya mana?\n\ncontoh:\n${usedPrefix}${command} https://vm.tiktok.com/ZGJAmhSrp/`
-    tiktokdlv3(args[0]).then(r => {
-    let video = r.video.no_watermark
-    conn.sendFile(m.chat, video, '', `*${wm}*`, m)
-    })
+const axios = require('axios')
+const cheerio = require('cheerio')
+var handler = async (m, { conn, args}) => {
+  if (!args[0]) throw 'Uhm...url nya mana?'
+    const { thumbnail, video, audio } = await tiktokdl(args[0])
+    const url = video
+    if (!url) throw 'Can\'t download video!'
+    await conn.sendMessage(m.chat, { video: { url: url }},m) 
 }
 handler.help = ['tiktok'].map(v => v + ' <url>')
 handler.tags = ['downloader']
-handler.limit = true
-handler.group = false
 
-handler.command = /^(tt|tiktok|tik)$/i
+handler.command = /^t(iktok(d(own(load(er)?)?|l))?|td(own(load(er)?)?|l))tt$/i
 
-module.exports = handler
+module.exports = handler 
+
+async function tiktokdl (url) {
+    if (!/tiktok/.test(url)) return error.link;
+    const gettoken = await axios.get("https://tikdown.org/id");
+    const $ = cheerio.load(gettoken.data);
+    const token = $("#download-form > input[type=hidden]:nth-child(2)").attr(
+        "value"
+    );
+    const param = {
+        url: url,
+        _token: token,
+    };
+    const {
+        data
+    } = await axios.request("https://tikdown.org/getAjax?", {
+        method: "post",
+        data: new URLSearchParams(Object.entries(param)),
+        headers: {
+            "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+            "user-agent": "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Safari/537.36",
+        },
+    });
+    var getdata = cheerio.load(data.html);
+    if (data.status) {
+        return {
+            status: true,
+            thumbnail: getdata("img").attr("src"),
+            video: getdata("div.download-links > div:nth-child(1) > a").attr("href"),
+            audio: getdata("div.download-links > div:nth-child(2) > a").attr("href"),
+        };
+    } else
+        return {
+            status: false,
+        };
+};
